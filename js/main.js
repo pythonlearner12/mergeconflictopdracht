04845d33368e21db6a1c62ac119b6bb605f2cead
Fred <36563@ma-web.nl>
2023-10-16 09:43:13 +0200

    string 1 veranderd

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ newgreeting.newGreeting()
 
 function basicHaiku()
 {
-    return ["Toward those short trees","We saw a batman descending","On a day in spring."]
+    return ["lopen langs de bomen","We saw a batman descending","On a day in spring."]
 }
 //zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
 
